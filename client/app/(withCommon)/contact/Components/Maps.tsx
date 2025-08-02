54381a4916bc7e9bb1f008ec4a3d822f272be124
developerMasum <developer.masum2001@gmail.com>
2025-08-02 11:50:57 +0600

    fixed map

--- client/app/(withCommon)/contact/Components/Maps.tsx
+++ client/app/(withCommon)/contact/Components/Maps.tsx
@@ -1,21 +1,24 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import { useState } from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import { Fade } from "react-awesome-reveal";
 
-// Latitude and longitude for Stockton
-const defaultCenter = { lat: 37.9577, lng: -121.2908 };
+const defaultCenter = { lat: 43.0037, lng: -75.6071 };
 
-// Custom map icon
-const customIcon = new L.Icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
+const containerStyle = {
+  width: "100%",
+  height: "60vh",
+};
 
 const Maps = () => {
+  const [mapApiLoaded, setMapApiLoaded] = useState(false);
+  const [mapsObject, setMapsObject] = useState<typeof google.maps | null>(null);
+
   return (
     <div className="h-[80vh] w-full max-w-[95vw] md:max-w-[90vw] mx-auto p-2 md:p-4 space-y-4 bg-transparent">
       <Fade>
@@ -23,26 +26,45 @@ const Maps = () => {
           Our Geographical Location
         </h1>
       </Fade>
-      <MapContainer
-        center={[defaultCenter.lat, defaultCenter.lng]}
-        zoom={7}
-        className="h-[60vh] md:h-[80vh] w-full rounded-sm"
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
 
-        {/* Static marker for Stockton */}
-        <Marker
-          position={[defaultCenter.lat, defaultCenter.lng]}
-          icon={customIcon}
+      <LoadScript
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+        onLoad={() => {
+          setMapApiLoaded(true);
+          setMapsObject(window.google.maps);
+        }}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={defaultCenter}
+          zoom={12}
+          options={{
+            fullscreenControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+          }}
         >
-          <Popup>
-            <p>Our Gym Centre</p>
-          </Popup>
-        </Marker>
-      </MapContainer>
+          {mapApiLoaded && mapsObject && (
+            <Marker
+              position={defaultCenter}
+              icon={{
+                url: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+                scaledSize: new mapsObject.Size(25, 25), // smaller icon size
+              }}
+            >
+              <InfoWindow position={defaultCenter}>
+                <div>
+                  <p>
+                    901 N Pitt St., Suite 170
+                    <br />
+                    Alexandria, NY, USA
+                  </p>
+                </div>
+              </InfoWindow>
+            </Marker>
+          )}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 };
